897e1f92e75627c9ee5295a4dd357c8498c6477b
AlexBoulyga <alex_boulyga@yahoo.de>
2015-07-09 11:46:21 +0200

    update 3

--- webstorm/elastsearchApp.js
+++ webstorm/elastsearchApp.js
@@ -1,3 +1,41 @@
-/**
- * Created by work on 7/8/15.
- */
+
+
+var elastsearchApp=angular.module('elastsearchApp', ['elasticsearch'])
+elastsearchApp.service('client',function(esFactory) {
+        return esFactory ({
+            host: 'localhost:9200'
+        });
+    });
+elastsearchApp.controller('elastCtrl', function($scope, client) {
+    var elastCtrl = this;
+    elastCtrl.name='hehe';
+    $scope.namex='scope geht';
+    $scope.movies = [];
+    $scope.testmovies = [
+        {'Title': 'film 1',
+            'Plot': 'plot 1'},
+        {'Title': 'film 2',
+            'Plot': 'plot 2'},
+        {'Title': 'film 3',
+            'Plot': 'plot 3'}
+    ];
+
+
+    client.search({
+        index: 'library',
+        type: 'movies',
+        body: {
+            query: {
+                match: {
+                    Title: 'Terminator'
+                    }
+                }
+            }
+    }).then(function(resp) {
+        movies = resp.hits.hits;
+    }, function (err) {
+        console.trace(err.message);
+    })  ;
+
+});
+
