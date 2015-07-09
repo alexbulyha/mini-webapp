8c300981dd6005b75dbf3f7954e2d9d642612902
AlexBoulyga <alex_boulyga@yahoo.de>
2015-07-09 12:56:13 +0200

    added searchbox, elasticsearch still not working here

--- webstorm/elastsearchApp.js
+++ webstorm/elastsearchApp.js
@@ -7,10 +7,45 @@ elastsearchApp.service('client',function(esFactory) {
         });
     });
 elastsearchApp.controller('elastCtrl', function($scope, client) {
+
     var elastCtrl = this;
-    elastCtrl.name='hehe';
-    $scope.namex='scope geht';
+
     $scope.movies = [];
+    elastCtrl.name='hehe';
+
+    elastCtrl.search = function () {
+        $scope.lastsearchterm = elastCtrl.searchterm;
+        client.search({
+            index: 'library',
+            type: 'movies',
+            body: {
+                "query": {
+                    "match": {
+                        "Title": elastCtrl.searchterm
+                    }
+                }
+            }
+        }).then(function(resp) {
+            movies = resp.hits.hits;
+        }, function (err) {
+            console.trace(err.message);
+        })  ;
+        elastCtrl.searchterm = '';
+
+        client.count({
+            index: 'library'
+        }, function (response) {
+            $scope.number=response.count;
+        }, function (err) {
+            console.trace(err.message);
+            $scope.number =0;
+        });
+    };
+
+
+
+
+
     $scope.testmovies = [
         {'Title': 'film 1',
             'Plot': 'plot 1'},
@@ -21,21 +56,19 @@ elastsearchApp.controller('elastCtrl', function($scope, client) {
     ];
 
 
-    client.search({
+
+
+
+
+
+    client.get({
         index: 'library',
         type: 'movies',
-        body: {
-            query: {
-                match: {
-                    Title: 'Terminator'
-                    }
-                }
-            }
-    }).then(function(resp) {
-        movies = resp.hits.hits;
-    }, function (err) {
-        console.trace(err.message);
-    })  ;
+        id: 1
+    }, function (error, response) {
+        $scope.movie1 = response;
+    });
+
 
 });
 
